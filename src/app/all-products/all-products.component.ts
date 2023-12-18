6d7bbe4f4791f7382792652704d822682bd5ee60
Basith-m <abm2k20@gmail.com>
2023-12-18 13:02:55 +0530

    get all products done

--- src/app/all-products/all-products.component.ts
+++ src/app/all-products/all-products.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-all-products',
   templateUrl: './all-products.component.html',
   styleUrls: ['./all-products.component.css']
 })
-export class AllProductsComponent {
+export class AllProductsComponent implements OnInit {
+  allProducts:any = []
+  constructor(private api:ApiService){}
 
+  ngOnInit(): void {
+    this.api.getAllProductsAPI().subscribe((res:any)=>{
+      this.allProducts = res
+    })
+  }
 }
